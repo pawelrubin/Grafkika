1f7c808981894cdfea7bb7478d5c86a48c063a55
pawelrubin <pawelrubin19@gmail.com>
2019-11-09 10:14:24 +0100

    Zadanie 1 dokonczone

--- l01/zad1/turtle.js
+++ l01/zad1/turtle.js
@@ -3,13 +3,26 @@ import { Turtle } from "./../Turtle.js"
 window.addEventListener('load', () => {
     const logoCanvas = document.getElementById("logoCanvas");
     const controlInput = document.getElementById("controlInput");
-    const turtle = new Turtle(logoCanvas)
-    
+    const execButton = document.getElementById("execButton");
+    const turtle = new Turtle(logoCanvas);
+    const coords = document.getElementById("coords");
+    const updateCoords = () => {
+        coords.textContent = `x: ${turtle.x_pos.toFixed(2)} y: ${turtle.y_pos.toFixed(2)} r: ${turtle.rotation.toFixed(2)}`;
+    };
+    const exec = () => {
+        turtle.parseInput(controlInput.value);
+        controlInput.value = '';
+        updateCoords();
+    };
+
+    updateCoords();
+
+    execButton.addEventListener('click', exec);
+
     controlInput.addEventListener('keyup', (event) => {
         event.preventDefault();
         if (event.key === 'Enter') {
-            turtle.parseInput(controlInput.value);
-            controlInput.value = '';
+            exec();
         }
-    })
+    });
 })
