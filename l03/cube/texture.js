cf85b968c106a584ee10bef78da30ba3e82d9141
Paweł Rubin <pawelrubin19@gmail.com>
2020-01-19 20:37:03 +0100

    Zadanie 1

---
+++ l03/cube/texture.js
@@ -0,0 +1,23 @@
+
+function loadTexture(gl, filter) {
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(
+    gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA,
+    gl.UNSIGNED_BYTE, new Uint8Array([0, 255, 0, 255])
+  );
+
+  const image = new Image();
+  image.src = "./cobble.png";
+  image.addEventListener('load', () => {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, filter);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, filter);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.generateMipmap(gl.TEXTURE_2D);
+  });
+
+  return texture;
+}
